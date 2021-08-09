5992ec75ea77a3a146aba78b06656651f3460a45
reskul.horst <reskul.horst@hotmail.com>
2021-08-09 16:30:25 +0200

    added expanduser to fix problem with '~' in path; added Configurator for loading and saving settings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 import platform
 from pathlib import Path
+from cfg import *
 
 from qtgui import *
 from PyQt5.QtWidgets import *
@@ -14,17 +15,19 @@ if __name__ == '__main__':
 
     operating_system = platform.system()
 
-    # working_path = Path("/MGB")
-    # if operating_system == 'Linux':
-    #     if not os.path.exists(working_path):
-    #         os.makedirs(working_path)
-    #     else:
-    #         print(f"{working_path} already exists.")
+    working_path = os.path.expanduser("~/Documents/MGB")
+    if operating_system == 'Linux':
+        if not os.path.exists(working_path):
+            os.makedirs(working_path)
+        else:
+            print(f"{working_path} already exists.")
+
+    cfg = Configurator(working_path)
 
     size = app.primaryScreen().size()
     x = (size.width() / 2) - (DEFAULT_WIDTH / 2)
     y = (size.height() / 2) - (DEFAULT_HEIGHT / 2)
-    mw = Window(x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT, operating_system)
+    mw = Window(x, y, DEFAULT_WIDTH, DEFAULT_HEIGHT, operating_system, cfg=cfg)
     mw.show()
 
     sys.exit(app.exec())
