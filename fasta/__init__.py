9fb5cc0085fe2fb0b55856d3ca215042f7056aa3
reskul.horst <reskul.horst@hotmail.com>
2021-08-09 23:06:16 +0200

    changed filename

--- fasta/__init__.py
+++ fasta/__init__.py
@@ -1,6 +1,6 @@
 from .fasta_header import *
 from .fasta_reader import *
 from .sequence import *
-from .marker_genes import *
+from .data_constructs import *
 
 __all__ = ['FastaHeaderUniProtKB', 'FastaHeaderNCBI', 'FastaReader', 'Sequence', 'MarkerGene']
--- fasta/marker_genes.py
+++
@@ -1,9 +0,0 @@
-class MarkerGene:
-    MG_number = None
-    contigs = []
-
-    def __init__(self, number):
-        self.MG_number = number
-
-    def add_contig(self, contig):
-        self.contigs.append(contig)
