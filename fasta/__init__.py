685adb07c5e4a3f3571e26570ed7baaaa9681201
reskul.horst <reskul.horst@hotmail.com>
2021-08-09 22:44:06 +0200

    updated for marker_genes.py

--- fasta/__init__.py
+++ fasta/__init__.py
@@ -1,5 +1,6 @@
 from .fasta_header import *
 from .fasta_reader import *
 from .sequence import *
+from .marker_genes import *
 
-__all__ = ['FastaHeaderUniProtKB', 'FastaHeaderNCBI', 'FastaReader', 'Sequence']
+__all__ = ['FastaHeaderUniProtKB', 'FastaHeaderNCBI', 'FastaReader', 'Sequence', 'MarkerGene']
