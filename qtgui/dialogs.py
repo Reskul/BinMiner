2b9d6acf089c308f40b0a0361584e14bcb5c3c3e
reskul.horst <reskul.horst@hotmail.com>
2021-08-11 16:53:15 +0200

    renamed & moved BinInfoDialog class in here

--- qtgui/dialogs.py
+++ qtgui/dialogs.py
@@ -193,3 +193,20 @@ class PathsDialog(QDialog):
 
     def cancel_clicked(self):
         self.close()
+
+
+class BinInfoDialog(QDialog):
+    def __init__(self, parent: QMainWindow, selected, debug=False):
+        super().__init__(parent)
+        self.setWindowTitle("Selected Bin")
+        self.DEBUG = False
+        self.data = selected
+
+        self.update_gui()
+
+    def update_selected(self, selected):
+        self.data = selected
+        self.update_gui()
+
+    def update_gui(self):
+        pass
