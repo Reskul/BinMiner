493d8cbc12c5de9e1c4dbf0109dbebaa3dc22b04
reskul.horst <reskul.horst@hotmail.com>
2021-08-11 16:37:45 +0200

    updated

--- qtgui/BinInfoDialog.py
+++ qtgui/BinInfoDialog.py
@@ -2,6 +2,17 @@ from PyQt5.QtWidgets import *
 
 
 class BinInfoDialog(QDialog):
-    def __init__(self, parent: QMainWindow):
+    def __init__(self, parent: QMainWindow, selected, debug=False):
         super().__init__(parent)
         self.setWindowTitle("Selected Bin")
+        self.DEBUG = False
+        self.data = selected
+
+        self.update_gui()
+
+    def update_selected(self, selected):
+        self.data = selected
+        self.update_gui()
+
+    def update_gui(self):
+        pass
