f41c1371fca9946c171c9f23cec4e39acd971c22
reskul.horst <reskul.horst@hotmail.com>
2021-08-14 14:52:16 +0200

    updated to show more info

--- test/test_npy_label.py
+++ test/test_npy_label.py
@@ -3,7 +3,13 @@ import numpy as np
 labels = np.load("/home/rom/Dokumente/BA_Data/precomputed/10s_labels.npy")
 access = np.load("/home/rom/Dokumente/BA_Data/precomputed/10s_access.npy")
 
-for l in access:
-    print(l)
+print("Access:")
+for a in access:
+    print(a)
 
 print(len(access))
+
+for l in labels:
+    print(l)
+
+print(len(labels))
