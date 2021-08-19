8774b572c6bab1bb9233da865b6bb3474641a5a7
reskul.horst <reskul.horst@hotmail.com>
2021-08-19 11:51:51 +0200

    deleted

--- fasta/__init__.py
+++
@@ -1,6 +0,0 @@
-from .fasta_header import *
-from .fasta_reader import *
-from .sequence import *
-from .data_constructs import *
-
-__all__ = ['FastaHeaderUniProtKB', 'FastaHeaderNCBI', 'FastaReader', 'Sequence', 'MarkerGene', 'Contig']
