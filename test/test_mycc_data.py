edef0d50a03be2021d4e42e5b2b3490393996358
reskul.horst <reskul.horst@hotmail.com>
2021-08-23 17:22:04 +0200

    some fucking around

--- test/test_mycc_data.py
+++ test/test_mycc_data.py
@@ -5,10 +5,13 @@ reader = FastaReader(FastaReader.MYCC)
 contigs = "/home/rom/Dokumente/BA_Data/mycc/10s/10s.fasta"
 depths = "/home/rom/Dokumente/BA_Data/mycc/10s/10s.depth.txt"
 
-depths_file = open(depths, 'r')
+assembly_25s = "/home/rom/Dokumente/BA_Data/mycc/25s/assembly.fa"
+
+# depths_file = open(depths, 'r')
 
 
-contigs_file = open(contigs, 'r')
-header = reader.read_raw_file(contigs_file)
-
+# contigs_file = open(contigs, 'r')
+assembly_file = open(assembly_25s,'r')
+header = reader.read_raw_file(assembly_file)
+test = reader.read_ma_fasta(assembly_file)
 print(header)
