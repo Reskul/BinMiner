7464019ed4790bbaf2f27c52345220d97b088ee6
romkarim.wemken <romkarim.wemken@stud.uni-goettingen.de>
2021-09-17 09:40:04 +0200

    completed argparser

--- lib/argparser.py
+++ lib/argparser.py
@@ -1,13 +1,24 @@
+import sys
+
+
 class ArgParser:
-    def __init__(self, args: dict):
+    def __init__(self, args: list):
         self.args = args
-        self.val = args
+        self.val = {}
 
     def parse(self, argv):
         for arg in argv[1:]:
-            if self.args[arg]:
+            if arg in self.args:
                 self.val[arg] = True
-        for key in self.val:
-            if type(self.val[key]) == str:
+        for key in self.args:
+            if key not in self.val:
                 self.val[key] = False
         return self.val
+
+
+if __name__ == "__main__":
+    expected_args = ['-d', '-t']
+    ap = ArgParser(expected_args)
+
+    given_args = ap.parse(sys.argv)
+    print(given_args)
