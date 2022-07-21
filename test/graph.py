4299b2f628ba398f9dcc2c3188c3ea157ca4b10f
romkarim.wemken <romkarim.wemken@stud.uni-goettingen.de>
2022-07-21 11:20:16 +0200

    tested some new graphs

--- test/graph.py
+++ test/graph.py
@@ -4,14 +4,17 @@ import numpy as np
 if __name__ == '__main__':
     print(np.log2(40), np.log2(40) ** 2)
 
-    n = 51
+    n = 100
     x = [i for i in range(n)[1:]]
     x_mean = sum(x) / len(x)
     print(x_mean)
     print(x)
     y = [(i ** 2) / x_mean for i in x]
-    y2 = [i ** 2 for i in x]
+    y2 = [(i ** 3) / 4 for i in x]
+    y3 = [np.log2(i) for i in x]
 
-    plt.plot(x, y)
-    plt.plot(x, y2)
+    # plt.plot(x, y, label='A')
+    plt.plot(x, y2, label='B')
+    plt.plot(x, y3, label='C')
+    plt.legend()
     plt.show()
